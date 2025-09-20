3f42725e37160c0825bf224fb3ce69670dae9be1
Pete MC-Mods <mcmods.pete@gmail.com>
2025-09-20 15:38:55 +0200

    more clean-up

--- src/main/java/net/petemc/warpedblocks/item/ModItems.java
+++ src/main/java/net/petemc/warpedblocks/item/ModItems.java
@@ -4,18 +4,11 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.petemc.warpedblocks.WarpedBlocks;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, WarpedBlocks.MOD_ID);
-
-    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
-            () -> new Item(new Item.Properties()));
-
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
